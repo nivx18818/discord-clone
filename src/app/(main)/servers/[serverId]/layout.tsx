a57e5e3e124183362ace280231e534c14c587dca
NguyenXuanVinh <nongami87@gmail.com>
2025-11-08 10:47:45 +0700

    feat: add layouts and pages for server and channel navigation
    - introduces main layout and server-specific layouts to structure the user interface, including sidebars and protected routes.
    - adds initial pages for server channels and direct conversations, handling authentication and redirect logic for improved navigation.
    - enhances user experience by ensuring only authorized users can access server content and are directed to appropriate channels.

---
+++ src/app/(main)/servers/[serverId]/layout.tsx
@@ -0,0 +1,41 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
+export default async function ServerIdLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) {
+  const { redirectToSignIn } = await auth();
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) return redirect("/");
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+}
---
+++ src/app/(main)/servers/[serverId]/page.tsx
@@ -0,0 +1,43 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+export default async function ServerIdPage({ params }: ServerIdPageProps) {
+  const { redirectToSignIn } = await auth();
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general"
+        },
+        orderBy: { createdAt: "asc" }
+      }
+    }
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
+}
