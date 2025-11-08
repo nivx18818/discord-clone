07ab2023c2369cd4bc44379847513f6daecea637
NguyenXuanVinh <nongami87@gmail.com>
2025-11-08 10:48:07 +0700

    feat: add invite code join and redirect flow
    - implements logic to allow users to join a server via invite code.
    - redirects authenticated users to sign-in, home, or the server page as appropriate.
    - ensures users can't join a server multiple times and creates a membership if needed.

---
+++ src/app/invite/[inviteCode]/page.tsx
@@ -0,0 +1,50 @@
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+
+interface InviteCodPageProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+export default async function InviteCodPage({
+  params: { inviteCode },
+}: InviteCodPageProps) {
+  const { redirectToSignIn } = await auth();
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  if (!inviteCode) return redirect("/");
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) return redirect(`/servers/${existingServer.id}`);
+
+  const server = await db.server.update({
+    where: {
+      inviteCode,
+    },
+    data: {
+      members: {
+        create: [{ profileId: profile.id }],
+      },
+    },
+  });
+
+  if (server) return redirect(`/servers/${server.id}`);
+
+  return null;
+}
